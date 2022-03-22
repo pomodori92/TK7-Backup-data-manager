ee326251a4b32d1e0273936851a060d34aa4cc89
pomodori92 <45762339+pomodori92@users.noreply.github.com>
2022-03-22 16:43:20 +0100

    Feat use envionmental Windows path

--- renderer.js
+++ renderer.js
@@ -6,65 +6,72 @@
 // process.
 
 const { ipcRenderer } = require('electron');
-const fs = require('fs-extra');
-const username = require("os").userInfo().username;
+const fse = require('fs-extra');
 const openExplorer = require('open-file-explorer');
 
-const win32TekkenPath = '%LOCALAPPDATA%\\TekkenGame\\Saved\\SaveGames\\TEKKEN7';
-const win32BackupPath = win32TekkenPath+'SaveBackup';
-const unixTekkenPath = '/Users/'+username+'/AppData/Local/TekkenGame/Saved/SaveGames/TEKKEN7/';
-const unixBackupPath = '/TekkenSaveBackup';
+const tekkenPath = process.env.LOCALAPPDATA + '\\TekkenGame\\Saved\\SaveGames\\TEKKEN7';
+const backupPath = process.env.HOMEPATH + '\\Saved Games\\TEKKEN7';
+
 
 function openBackupDir() {
-    openExplorer(win32BackupPath, error => {
-        if (error)
-            snack(error, 'error');
-    });
+	openExplorer(backupPath, (error) => {
+		if (error)
+			snack(error, "error");
+	});
 }
+
 
 function openTekkenDir() {
-    openExplorer(win32TekkenPath, error => {
-        if (error)
-            snack(error, 'error');
-    });
+	openExplorer(tekkenPath, (error) => {
+		if (error)
+			snack(error, "error");
+	});
 }
+
 
 function backupSave() {
-    try {
-        fs.copySync(unixTekkenPath, unixBackupPath);
-        snack('Success!');
-    } catch (error) {
-        snack(error, 'error');
-    }
+	try {
+		fse.copySync(tekkenPath, backupPath);
+		snack("Success!");
+	} catch (error) {
+		snack(error, "error");
+	}
 }
+
 
 function importSave() {
-    try {
-        fs.copySync(unixBackupPath, unixTekkenPath);
-        snack('Success!');
-    } catch (error) {
-        snack(error, 'error');
-    }
+	try {
+		fse.copySync(backupPath, tekkenPath);
+		snack("Success!");
+	} catch (error) {
+		snack(error, "error");
+	}
 }
 
-function snack(message, messageType = '') {
-    if (!message)
-        return;
 
-    switch (messageType.toLowerCase()) {
-        case 'error':
-            console.error(message);
-            break;
-        default:
-            console.log(message);
-    }
-    // Get the snackbar DIV
-    let snack = document.getElementById("snackbar");
-    snack.innerText = message;
-    // Add the "show" class to DIV
-    snack.className = "show";
-    // After 3 seconds, remove the show class from DIV
-    setTimeout(function() {
-        snack.className = snack.className.replace("show", "");
-    }, 3000);
+function snack(message, messageType = "") {
+	if (!message)
+		return;
+
+	switch (messageType.toLowerCase()) {
+		case "error":
+			console.error(message);
+			break;
+
+		case "info":
+			console.info(message);
+			break;
+
+		default:
+			console.log(message);
+	}
+	// Get the snackbar DIV
+	let snack = document.getElementById("snackbar");
+	snack.innerText = message;
+	// Add the "show" class to DIV
+	snack.className = "show";
+	// After 3 seconds, remove the show class from DIV
+	setTimeout(function () {
+		snack.className = snack.className.replace("show", "");
+	}, 3000);
 }
