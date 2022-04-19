ffdde704605607f385f86fabd10b80abca33f597
pomodori92 <thekhameleon92@gmail.com>
2022-04-19 23:19:48 +0200

    Removed open-file-explorer and replaced ipcRenderer.

--- renderer.js
+++ renderer.js
@@ -5,73 +5,68 @@
 // selectively enable features needed in the rendering
 // process.
 
-const { ipcRenderer } = require('electron');
+const { shell } = require('electron');
 const fse = require('fs-extra');
-const openExplorer = require('open-file-explorer');
 
-const tekkenPath = process.env.LOCALAPPDATA + '\\TekkenGame\\Saved\\SaveGames\\TEKKEN7';
-const backupPath = process.env.HOMEPATH + '\\Saved Games\\TEKKEN7';
-
+const tekkenPath = `${process.env.LOCALAPPDATA}\\TekkenGame\\Saved\\SaveGames\\TEKKEN7`;
+const backupPath = `${process.env.USERPROFILE}\\Saved Games\\TEKKEN7`;
 
 function openBackupDir() {
-	openExplorer(backupPath, (error) => {
-		if (error)
-			snack(error, "error");
+	shell.openPath(backupPath).then((error) => {
+    if (error)
+		snack(error, 'error');
 	});
 }
-
 
 function openTekkenDir() {
-	openExplorer(tekkenPath, (error) => {
-		if (error)
-			snack(error, "error");
+	shell.openPath(backupPath).then((error) => {
+	if (error)
+		snack(error, 'error');
 	});
 }
-
 
 function backupSave() {
 	try {
 		fse.copySync(tekkenPath, backupPath);
-		snack("Success!");
+		snack('Backupped successfully!');
 	} catch (error) {
-		snack(error, "error");
+		snack(error, 'error');
 	}
 }
-
 
 function importSave() {
 	try {
 		fse.copySync(backupPath, tekkenPath);
-		snack("Success!");
+		snack('Saves imported!');
 	} catch (error) {
-		snack(error, "error");
+		snack(error, 'error');
 	}
 }
 
-
-function snack(message, messageType = "") {
+function snack(message, messageType = '') {
 	if (!message)
 		return;
 
 	switch (messageType.toLowerCase()) {
-		case "error":
+		case 'error':
 			console.error(message);
 			break;
 
-		case "info":
+		case 'info':
 			console.info(message);
 			break;
 
 		default:
 			console.log(message);
 	}
+
 	// Get the snackbar DIV
-	let snack = document.getElementById("snackbar");
+	let snack = document.getElementById('snackbar');
 	snack.innerText = message;
-	// Add the "show" class to DIV
-	snack.className = "show";
+	// Add the 'show' class to DIV
+	snack.className = 'show';
 	// After 3 seconds, remove the show class from DIV
 	setTimeout(function () {
-		snack.className = snack.className.replace("show", "");
+		snack.className = snack.className.replace('show', '');
 	}, 3000);
 }
